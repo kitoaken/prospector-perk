ac0b3e2ce2e81bfcb396a21374c7394a7d7b651d
kitoaken <102918484+kitoaken@users.noreply.github.com>
2025-05-01 17:48:02 +0100

    Updated languages

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 local translations = ModTextFileGetContent( "data/translations/common.csv" ) or ""
-translations = translations .. ModTextFileGetContent( "mods/prospector-perk/files/translations/en.csv" )
+translations = translations .. ModTextFileGetContent( "mods/prospector-perk/files/translations/common-append.csv" )
 ModTextFileSetContent( "data/translations/common.csv", translations )
 
 ModLuaFileAppend("data/scripts/perks/perk_list.lua", "mods/prospector-perk/files/scripts/perk_list.lua")
