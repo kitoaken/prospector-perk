0e8479c47f6e6cb448d7b7cd09807679079afdce
kitoaken <102918484+kitoaken@users.noreply.github.com>
2025-05-02 01:51:38 +0100

    Flag to prevent multiple pickups

--- files/scripts/perk_list.lua
+++ files/scripts/perk_list.lua
@@ -8,19 +8,21 @@ table.insert(perk_list,
     stackable = STACKABLE_NO,
     usable_by_enemies = false,
     func = function( entity_perk_item, entity_who_picked, item_name )
-      GameAddFlagRun("PERK_PICKED_PROSPECTOR")
-        local x,y = EntityGetTransform( entity_who_picked )
-        local child_id = EntityLoad( "mods/prospector-perk/files/entities/misc/perks/prospector.xml", x, y )
-        EntityAddTag( child_id, "perk_entity" )
-        EntityAddChild( entity_who_picked, child_id )
+      if not GameHasFlagRun("PERK_PICKED_PROSPECTOR") then
+        GameAddFlagRun("PERK_PICKED_PROSPECTOR")
+          local x,y = EntityGetTransform( entity_who_picked )
+          local child_id = EntityLoad( "mods/prospector-perk/files/entities/misc/perks/prospector.xml", x, y )
+          EntityAddTag( child_id, "perk_entity" )
+          EntityAddChild( entity_who_picked, child_id )
 
-        EntityAddComponent( child_id, "LuaComponent", 
-        {
-            script_source_file = "mods/prospector-perk/files/scripts/perks/prospector.lua",
-            -- This could be every few frames, but there'd be slower gold sounds and some desync with
-            -- picking up gold vs it being added to the player wallet
-            execute_every_n_frame = "1",
-        } )
+          EntityAddComponent( child_id, "LuaComponent", 
+          {
+              script_source_file = "mods/prospector-perk/files/scripts/perks/prospector.lua",
+              -- This could be every few frames, but there'd be slower gold sounds and some desync with
+              -- picking up gold vs it being added to the player wallet
+              execute_every_n_frame = "1",
+          } )
+      end
     end,
   }
 )
