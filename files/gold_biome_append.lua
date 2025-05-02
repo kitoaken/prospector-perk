87d8f14602f39860dfb38f48199714ab48156b4f
kitoaken <102918484+kitoaken@users.noreply.github.com>
2025-05-02 02:10:58 +0100

    Make altar still spawn if player has perk

--- files/gold_biome_append.lua
+++ files/gold_biome_append.lua
@@ -4,10 +4,10 @@ dofile_once("data/scripts/perks/perk.lua")
 RegisterSpawnFunction( 0xffffeedd, "init" )
 
 function init(x, y, w, h)
+    local altar = "mods/prospector-perk/files/biome_impl/prospector_altar.png"
+    local altar_visual = "mods/prospector-perk/files/biome_impl/prospector_altar_visual.png"
+    LoadPixelScene( altar, altar_visual , x+222, y+500, "", true )
     if not GameHasFlagRun("PERK_PICKED_PROSPECTOR") then
-        local altar = "mods/prospector-perk/files/biome_impl/prospector_altar.png"
-        local altar_visual = "mods/prospector-perk/files/biome_impl/prospector_altar_visual.png"
-        LoadPixelScene( altar, altar_visual , x+222, y+500, "", true )
         perk_spawn( x + 256, y + 490, "ALFR_PROSPECTOR")
     end
 end
