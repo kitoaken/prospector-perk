ac95a8c77131dd43df4549ff4d9a86d51950e08b
UserK <111083281+FatalUserK@users.noreply.github.com>
2025-08-28 09:24:40 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,28 +1,32 @@
 --adding translations to the game without slightly breaking stuff is annoying, this code here avoids breaking it and unbreaks it if someone else broke it before us, similar to code used in Apotheosis i think
 local translations = ModTextFileGetContent("data/translations/common.csv")
-local new_translations = ModTextFileGetContent("mods/prospector-perk/standard.csv")
-translations = translations .. "\n" .. new_translations .. "\n"
+translations = translations .. "\n" .. ModTextFileGetContent("mods/prospector-perk/standard.csv") .. "\n"
 translations = translations:gsub("\r", ""):gsub("\n\n+", "\n")
 ModTextFileSetContent("data/translations/common.csv", translations)
 
-
+--add perk
 ModLuaFileAppend("data/scripts/perks/perk_list.lua", "mods/prospector-perk/files/perk/append_perk_list.lua")
-if not ModSettingGet("prospector-perk.spawn_gold_room") then GameAddFlagRun("PROSPECTOR_NO_GOLD_BIOME") end --set flag to not spawn perk in gold if setting isnt set
+
 
 local nxml = dofile_once("mods/prospector-perk/lib/nxml.lua")
-for xml in nxml.edit_file("data/biome/gold.xml") do
-    local topology = xml:first_of("Topology")
-    if topology then
-        if topology.attr.lua_script ~= nil then
-            ModLuaFileAppend(topology.attr.lua_script, "mods/prospector-perk/files/biome/gold_biome.lua")
-        else
-            topology.attr.lua_script ="mods/prospector-perk/files/biome/gold_biome.lua"
+
+--dont spawn perk in gold if setting isnt set
+if ModSettingGet("prospector-perk.spawn_gold_room") then
+    for xml in nxml.edit_file("data/biome/gold.xml") do
+        local topology = xml:first_of("Topology")
+        if topology then
+            if topology.attr.lua_script ~= nil then
+                ModLuaFileAppend(topology.attr.lua_script, "mods/prospector-perk/files/biome/gold_biome.lua")
+            else
+                topology.attr.lua_script ="mods/prospector-perk/files/biome/gold_biome.lua"
+            end
         end
     end
 end
 
+--use [wallet_money] for more ideal compatibility
 for xml in nxml.edit_file("data/materials.xml") do
 	for elem in xml:each_child() do
-        if elem.attr.name == "gold" or elem.attr.name == "gold_radioactive" then elem.attr.tags = elem.attr.tags .. ",[wallet_money]" end --use wallet money for more ideal compatibility
+        if elem.attr.name == "gold" or elem.attr.name == "gold_radioactive" then elem.attr.tags = elem.attr.tags .. ",[wallet_money]" end
     end
 end
